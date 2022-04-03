8b4c5316721cb53b791e8fd65a011118e8660d96
wlillisdev <williamlillis100@gmail.com>
2022-04-03 08:18:35 +0000

    inital commit

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,21 @@
  */
 
 // Regular function call 
+const sumAll = (a,b,c) => a + b + c;
+let sum = sumAll(1,2,3);
+console.log ("sum", sum)
 
 // Extra arguments are ignored
 
 // Function using ...rest
+
+const sumRest = (a, b, c, ...rest) => {
+
+    let sum = a + b + c;
+    for(let i of rest){
+        sum += i;
+    }
+    return sum;
+}
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log ("sum 3", sum3);
